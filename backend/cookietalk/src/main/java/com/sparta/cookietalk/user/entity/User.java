81b50f3ec7f00f372b44a10d464b3db1a2473bea
Nameless1004 <jjho1029@gmail.com>
2024-09-16 08:25:31 +0900

    [feat] user 엔티티 추가

---
+++ backend/cookietalk/src/main/java/com/sparta/cookietalk/user/entity/User.java
@@ -0,0 +1,30 @@
+package com.sparta.cookietalk.user.entity;
+
+import com.sparta.cookietalk.common.enums.UserRole;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Getter
+@Entity
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+}
