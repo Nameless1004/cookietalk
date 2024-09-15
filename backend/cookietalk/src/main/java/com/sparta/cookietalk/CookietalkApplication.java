f417ea8ef44e4f6f0020c0b76ff51e1ec69016eb
Nameless1004 <jjho1029@gmail.com>
2024-09-16 08:17:56 +0900

    [add] init project

---
+++ backend/cookietalk/src/main/java/com/sparta/cookietalk/CookietalkApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.cookietalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookietalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookietalkApplication.class, args);
+    }
+
+}
