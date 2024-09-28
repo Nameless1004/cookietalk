e8807492fa3b8a7a270a608f01190552c1fe6cdf
minyeong <minyk0512@gmail.com>
2024-09-29 04:13:18 +0900

    [style] PostCookie 페이지 레이아웃 수정

--- frontend/src/components/inputs/FormInput.jsx
+++ frontend/src/components/inputs/FormInput.jsx
@@ -1,10 +1,19 @@
-const FormInput = ({ name, label, type, className, formValue, setFormValue, ...props }) => {
+const FormInput = ({
+  name,
+  label,
+  type,
+  containerStyle = 'grid grid-cols-[1fr_2fr]',
+  className,
+  formValue,
+  setFormValue,
+  ...props
+}) => {
   const handleFormChange = (e) => {
     setFormValue({ ...formValue, [e.target.name]: e.target.value });
   };
 
   return (
-    <div className='grid grid-cols-[1fr_2fr]'>
+    <div className={containerStyle}>
       {label ? <span className='font-bold'>{label}</span> : null}
       <input
         name={name}
