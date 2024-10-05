aa573e628d38f5ac2ad936227173d3e263e015b2
minyeong <minyk0512@gmail.com>
2024-10-06 03:02:42 +0900

    [bugfix] signout accessToken 전달 수정

--- frontend/src/api/userRequest.js
+++ frontend/src/api/userRequest.js
@@ -25,7 +25,7 @@ export const postSignOut = async (accessToken) => {
       {},
       {
         headers: {
-          Authorization: { accessToken },
+          Authorization: accessToken,
         },
       },
     );
