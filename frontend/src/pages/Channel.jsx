1904ecdfba790a2d2113da4d4b3c9a1da6f90b4a
minyeong <minyk0512@gmail.com>
2024-10-09 20:35:56 +0900

    [feat] user Channel로 이동하는 기능 커스텀훅으로 분리

--- frontend/src/pages/Channel.jsx
+++ frontend/src/pages/Channel.jsx
@@ -1,29 +1,14 @@
 import { useLocation } from 'react-router-dom';
-import useUserStore from '../zustand/userStore.js';
 import { useGetChannelProfile } from '../query/channelQuery.js';
 import TemporalError from '../components/TemporalError.jsx';
-import { useEffect, useState } from 'react';
 import ChannelCookieRegion from '../components/channel/ChannelCookieRegion.jsx';
 import ChannelProfile from '../components/channel/ChannelProfile.jsx';
 import UserSeries from '../components/channel/UserSeries.jsx';
 
 const Channel = () => {
   const location = useLocation();
-  // const { isMyChannel } = location.state || { isMyChannel: false }; //TODO: url 직접 입력해 들어오는 경우도 내 채널로 인지하도록 수정
-  const { id } = useUserStore((state) => state.user);
-  const [isMyChannel, setIsMyChannel] = useState(false);
-
+  const { id, isMyChannel } = location.state;
   const { data, isPending: isProfilePending, isError, error, isSuccess } = useGetChannelProfile(id);
-
-  useEffect(() => {
-    if (isSuccess) {
-      console.log(data); // TODO: 로그 제거
-
-      if (data.userId === id) {
-        setIsMyChannel(true);
-      }
-    }
-  }, [isSuccess]);
 
   if (isError) {
     console.error(error);
